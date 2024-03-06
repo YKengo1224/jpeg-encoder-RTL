4f383cffeb02a3ada431ab6691b2b780b59b6b33
YKengo1224 <kengo_y_15@icloud.com>
2024-03-07 01:03:07 +0900

    [debug] add signal type [add.v]

--- rtl/add.v
+++ rtl/add.v
@@ -1,13 +1,13 @@
-`default_nettype none
+`default_nettype wire
 module add(
         a, 
         b, 
         s, 
         cout
 );
-        input  [3:0] a, b;
-        output [3:0] s;
-        output       cout;
+        input  wire [3:0] a, b;
+        output wire [3:0] s;
+        output wire       cout;
 
         assign {cout,s} = a + b;
 
